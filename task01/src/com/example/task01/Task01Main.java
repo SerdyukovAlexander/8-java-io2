f3e406123d7f52fcaa4344b63a9f28a5e1191f5d
Serdyukov Alexander <30432511+SerdyukovAlexander@users.noreply.github.com>
2024-12-06 11:22:09 +0500

    8 модуль Сердюков ПрИ-201

--- task01/src/com/example/task01/Task01Main.java
+++ task01/src/com/example/task01/Task01Main.java
@@ -1,20 +1,35 @@
 package com.example.task01;
 
-import java.io.File;
-import java.io.IOException;
+import java.io.*;
+import java.nio.channels.Pipe;
 
-public class Task01Main {
-    public static void main(String[] args) throws IOException, InterruptedException {
-        //здесь вы можете вручную протестировать ваше решение, вызывая реализуемый метод и смотря результат
-        // например вот так:
-
-        /*
+public class Task01Main
+{
+    public static void main(String[] args) throws IOException, InterruptedException
+    {
         System.out.println(extractSoundName(new File("task01/src/main/resources/3727.mp3")));
-        */
     }
 
-    public static String extractSoundName(File file) throws IOException, InterruptedException {
-        // your implementation here
-        return "sound name";
+    public static String extractSoundName(File file) throws IOException, InterruptedException
+    {
+        ProcessBuilder processBuilder = new ProcessBuilder();
+        processBuilder.command("ffprobe", "-v", "error", "-of", "flat", "-show_format", file.getName());
+        processBuilder.directory(file.getParentFile());
+
+
+        Process process = processBuilder.start();
+        process.waitFor();
+        try (BufferedReader reader = new BufferedReader(new InputStreamReader(process.getInputStream()))) {
+            while (reader.ready()) {
+                String line = reader.readLine();
+                if (line.contains("format.tags.title")) {
+                    line = line.split("=")[1].replace("\"", "");
+                    return line;
+                }
+                System.out.println(line);
+            }
+
+        }
+        return "Not found";
     }
 }
--- task02/src/com/example/task02/Task02Main.java
+++ task02/src/com/example/task02/Task02Main.java
@@ -1,23 +1,31 @@
 package com.example.task02;
 
+import java.nio.file.Files;
 import java.io.IOException;
 import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.ArrayList;
 import java.util.List;
+import java.util.stream.Stream;
 
-public class Task02Main {
-    public static void main(String[] args) throws IOException, InterruptedException {
-        //здесь вы можете вручную протестировать ваше решение, вызывая реализуемый метод и смотря результат
-        // например вот так:
-
-        /*
+public class Task02Main
+{
+    public static void main(String[] args) throws IOException, InterruptedException
+    {
         System.out.println(listFiles(Paths.get("task02/src/main/resources/")));
-        */
-
     }
 
-    public static List<Path> listFiles(Path rootDir) throws IOException, InterruptedException {
-        // your implementation here
+    public static List<Path> listFiles(Path rootDir) throws IOException, InterruptedException
+    {
+        List<Path> output = new ArrayList<>();
 
-        return null;
+        try(Stream<Path> stream = Files.walk(rootDir))
+        {
+            stream.forEach(p -> {
+                if (!p.toFile().isDirectory())
+                    output.add(p);
+            });
+        }
+        return output;
     }
 }
